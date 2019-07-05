98a2747c350e2630f7aa37f91d273fd19c48a711
Ed Coyle <Ed.Coyle@infor.com>
2019-07-05 11:25:05 -0400

    try removing weird config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require('@zeit/next-sass');
 const prod = process.env.NODE_ENV === 'production';
 
 const settings = withSass({
-  assetPrefix: prod ? '/EdwardCoyle.dev/' : ''
+  //assetPrefix: prod ? '/EdwardCoyle.dev/' : ''
 });
 
 // Use Sass when exporting
