b9413307fdd5995b14b7f2e27b628c72762ce24c
Ed Coyle <Ed.Coyle@infor.com>
2019-07-05 11:27:32 -0400

    removed useless config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const withSass = require('@zeit/next-sass');
 const prod = process.env.NODE_ENV === 'production';
 
-const settings = withSass({
-  //assetPrefix: prod ? '/EdwardCoyle.dev/' : ''
-});
+const settings = withSass({});
 
 // Use Sass when exporting
 module.exports = settings;
