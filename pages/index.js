2f07c135e60395f63c795f3cba364d82384d7310
Ed Coyle <Ed.Coyle@infor.com>
2019-07-05 10:26:06 -0400

    "reactify" the previous index page

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,24 @@ import Head from 'next/head';
 
 function Home() {
   return (
-    <Head>
-      <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700&display=swap" rel="stylesheet">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-18393322-3"></script>
-      <script src="/static/js/ga.js"></script>
-      <title>EdwardCoyle.dev | Coming Soon</title>
-    </Head>
-    <div className="main">
-      <h1>Welcome to Next.js</h1>
+    <div>
+      <Head>
+        <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700&display=swap" rel="stylesheet"/>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-18393322-3"></script>
+        <script src="/static/js/ga.js"></script>
+        <title>EdwardCoyle.dev | Coming Soon</title>
+      </Head>
+      <section id="main" className="main">
+        <div className="masthead">
+          <h1>Edward Coyle</h1>
+          <p className="masthead--bullets">
+            <span>Developer</span>
+            <span>Designer</span>
+            <span>Animator</span>
+          </p>
+          <p>Website Coming Soon</p>
+        </div>
+      </section>
     </div>
   )
 }
